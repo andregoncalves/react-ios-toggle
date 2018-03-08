8f0898bd58aa5f9d6dc5e14750d673d45132556b
Andre Goncalves <andre@andregoncalves.com>
2018-03-08 15:35:01 +0000

    include the dist folder

---
+++ dist/IosToggle.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="react" />
+import * as React from 'react';
+import './IosToggle.css';
+export interface IosToggleProps {
+    name: string | undefined;
+    value: string | undefined;
+    theme: string | undefined;
+    checked: boolean | undefined;
+    onChange: any | undefined;
+}
+export interface IosToggleState {
+    checked: boolean;
+}
+export default class IosToggle extends React.Component<IosToggleProps, IosToggleState> {
+    static defaultProps: {
+        checked: boolean;
+        theme: string;
+        value: string;
+    };
+    constructor(props: IosToggleProps);
+    _handleChange(e: React.ChangeEvent<HTMLInputElement>): void;
+    render(): JSX.Element;
+}
